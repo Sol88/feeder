47520e38a1bcf76635e614c2617d67deba9d8c49
Victor Zaikin <viczaikin@gmail.com>
2022-04-20 20:26:34 +0300

    Clearing of internal cache

--- feeder/Sources/Cache/Cache.swift
+++ feeder/Sources/Cache/Cache.swift
@@ -1,13 +1,24 @@
 import Foundation
+import UIKit
 
 final class Cache<Key: Hashable, Value> {
+	private let maxAmountOfElements = 15
+	private var datesForAddedCache: [CFTimeInterval: Key] = [:]
 	private var cache: [Key: Value] = [:]
 	private var lock = NSLock()
 
 	func insert(_ value: Value, forKey key: Key) {
 		defer { lock.unlock() }
 		lock.lock()
+		datesForAddedCache[CACurrentMediaTime()] = key
 		cache[key] = value
+
+		if datesForAddedCache.keys.count > maxAmountOfElements {
+			guard let key = datesForAddedCache.keys.sorted(by: <).first else { return }
+			if let keyToRemove = datesForAddedCache.removeValue(forKey: key) {
+				cache.removeValue(forKey: keyToRemove)
+			}
+		}
 	}
 
 	func value(forKey key: Key) -> Value? {
@@ -19,6 +30,10 @@ final class Cache<Key: Hashable, Value> {
 	func remove(forKey key: Key) {
 		defer { lock.unlock() }
 		lock.lock()
+
+		if let keyToRemove = datesForAddedCache.first(where: { $0.value == key }) {
+			datesForAddedCache.removeValue(forKey: keyToRemove.key)
+		}
 		cache.removeValue(forKey: key)
 	}
 
